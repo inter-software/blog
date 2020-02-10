4c7df2949ea4cf185f080be4a862586a000b622e
inter-software <intern-software@growthaccelerationpartners.com>
2020-02-10 10:43:47 -0500

    Actualizando cambios en la vista

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
-    http_basic_authenticate_with name: "admin", password: "123", 
-        except: [:index, :show]
+
+    #http_basic_authenticate_with name: "admin", password: "123", 
+    #except: [:index, :show] 
+ 
 
     def index
         @article = Article.all
