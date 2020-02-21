8e00e0c6c39aedfb68a333ec68e2a8fba2eabb07
inter-software <intern-software@growthaccelerationpartners.com>
2020-02-21 17:18:01 -0500

    Include some unit test with RSpec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,67 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord # rubocop:todo Style/Documentation
+
   belongs_to :article
-
-  # scope :active_comments, -> {where(active: true)}
-
-  # scope :inactive_comments, -> {where(active: false)}
 
   validates :commenter, presence: true
 
   validates :body, presence: true
+
+
+  def report_comment
+
+    "My Comments: #{commenter}, #{body}"
+
+  end
+  
+  def search_comment
+
+    ['Anna', 'carlos', 'charly', 'esteban'].include?(:commenter)
+
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,53 +3,94 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # It's method before(:context) invoked before the transaction is open.
-  # For example, use in speed by created data once before in group excute el block.
-  # before(:context) do
-  #   @comment = Comment.create #create to instance of the object Comment, call to create method
-  # end
 
-  # after(:context) do
-  #  @comment = Comment.destroy
-  # end
-
-  # before(:example_load) do
-  #  @comment.reload
-  # end
-
-  #
-  #       the instance variable @comment that recreated in the two examples,
-  #       so has different object. It's for each example define a new object-
-
-  # define a if implicite, execute block (true)
-  # it 'Does Something' do
-  #  #It's that equals with the instance variable, call do_something method
-  #  expect(@comment).to do_something
-  # end
-
-  # Declare un if with acction in else (False)
-  # it 'Does Something else' do
-  #  expect(@comment).to do_something_else
-  # end
-
-  #
-  #
-  #
-  #   Note: Even though database updates in each example will be rolled back, the
-  #   object won't know about those rollbacks so the object and its backing
-  #   data can easily get out of sync.
-  #
-
-  context 'With two or more comments' do
-    it 'Orders them in reverse chronologically' do
-      # declare the varible comment and assing in the object Comment invokend create method.
-      comment = Comment.create!
-      # created two objects, validation it created.
-      comment1 = comment.comments.create!(commenter: 'Pepe', body: 'First Comment')
-      comment2 = comment.comments.create!(commenter: 'Pepa', body: 'Second Comment')
-      # Declare method expect that have a if (Implicite),
-      # It's get with parameter la instance the objet Comment and reload the comments method.
-      expect(comment.reload.comments).to eq([comment2, comment1])
+  describe  "Some Examples Article Model" do
+    it 'Return the Comment Report ' do
+      comment = Comment.new(commenter: "Pepe", body: "Great!, Wonderfull")
+      info = comment.report_comment # "My Comments: Pepe, Great!, Wonderfull"
+      expect(info).to eq("My Comments: Pepe, Great!, Wonderfull") # eq es un matcher de rspec
     end
+
   end
+
+  describe  'Common, built-in expectation matchers' do
+
+    example "Equality" do
+      expect('x'+'y').to eq('xy') # x == y
+      expect('x'+'y').to eql('xy') # x.eql?(y)
+      expect('x'+'y').not_to be('xy') # x.equal?(y)
+    end
+
+    example "Strings" do
+      expect('abcd').to include('bc')
+      expect('abcd').to start_with 'ab'
+      expect('abcd').to end_with 'cd'
+      expect('abcd').to match /[a-z]+/
+    end
+
+    example "Collections" do
+      expect([1, 2, 3, 4, 5, 6]).to include(3,5)
+      expect([1, 3, 5, 7, 9, 11]).to contain_exactly(11, 9, 7, 5, 3, 1)
+      expect({name: "Juan", age: 2020-1950}).to include(age: 70)
+    end
+
+    example "Booleans and Nils" do
+      expect(true).to be(true)
+      expect(false).to be(false)
+      expect('abc').to be_truthy
+      expect(nil).to be_falsey
+      expect(nil).to be_nil
+    end
+
+    example "Numeric" do
+      expect(5).to be > 4
+      expect(5).to be >= 4
+      expect(5).to be < 6
+      expect(5).to be <= 6
+      expect(5).to be_between(4, 6).exclusive
+      expect(5).to be_between(5, 6).inclusive
+      expect(4.99).to be_within(0.02).of(5)
+    end
+
+    #example "Errors (Exceptions)" do
+    #  expect{5 / 0}.to raise_error(ZeroDivisionError)
+    #  expect{5 / 0}.to raise_error("Divided by 0")
+    #  expect{5 / 0}.to raise_error(ZeroDivisionError, "Divided by 0")
+    #end
+  end
+
+  describe "Predicate Matchers" do
+
+    example "Arrays" do
+      expect([]).to be_empty   #  [].empty? : It's empty
+    end
+
+    example "Hash" do
+      expect({a: 4}).to have_key(:a) #  {a:4}.hash_key? : It's key
+      expect({a: 4}).to have_value(4) #  {a:4}.hash_value? : It's value
+    end
+
+    example "Objects" do
+      expect(5).not_to be_nil # 5.nil? : The Object (5)  is nil
+      expect(5).to be_instance_of Fixnum # 5.instance_of?(Fixnum)
+      expect(5).to be_kind_of Numeric # 5.kind_of?
+    end
+
+    #it "Custom with Article Class" do
+
+    # comment = Comment.new(commenter: "Charly", body: "Great")
+    #  infos = comment.search_comment
+    # expect(infos).to eq('charly')
+
+    #end
+
+    example "Rarely used matcherns" do
+      expect(6).to respond_to(:odd?)
+      expect(Pathname.new('.')).to exist # pathname.exist? si es existe la ruta.
+      expect(1..10).to cover(7)
+    end
+
+
+  end
+
 end
