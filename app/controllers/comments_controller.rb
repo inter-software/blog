178666f08fee8cd2aa0179a3e56d8e0062dc66a5
inter-software <intern-software@growthaccelerationpartners.com>
2020-03-12 07:52:27 -0500

    add commit in article and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
-# frozen_string_literal: true
-
-# rubocop:todo Style/Documentation
 class CommentsController < ApplicationController
   # http_basic_authenticate_with name: "admin", password: "123",
   #    only: [:destroy]
+
+  before_action :set_comment, only: %i[edit update]
+
+  def index
+    @comments = Comment.all
+  end
 
   def show
     @article = Article.find(params[:article_id])
@@ -11,20 +14,24 @@ class CommentsController < ApplicationController
     redirect_to :show
   end
 
+  def edit
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
   def create
-    # al articulo con id = ?, fue encontrado, pasamos una variable de instancia,
-    # refencia la variable de instancia  @article con el articulo encontrado,
-    # crea un evento en comments, con el metodo create, le pasamos un serie de parametros.
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    @article = Article.find(params[:article_id]) # Recupera el articulo
+    @comment = @article.comments.create(comment_params) # le asinga ala instancia del objeto comment los parametros son enviados por post
     redirect_to article_path(@article)# /articles/:article_id/comments
   end
 
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
+    @article = Article.find(params[:article_id]) # get the ID the Article
+    @comment = @article.comments.find(params[:id]) # Get the id the comments in article.
+    @comment.destroy # The instance the comments, Call to the destroy method
 
     redirect_to article_path(@article)
   end
@@ -34,5 +41,10 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:commenter, :body)
     end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
 end
-# rubocop:enable Style/Documentation
+
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
 
-  belongs_to :article
-
   belongs_to :commentable, polymorphic: true
 
   validates :commenter, presence: true
